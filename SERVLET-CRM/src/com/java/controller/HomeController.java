6bfbe127a52661f85eaac8d30f956b75d4b78586
caogiahuy13 <caogiahuy13@gmail.com>
2019-06-23 21:37:41 +0700

    Database Connection with JDBC

--- SERVLET-CRM/src/com/java/controller/HomeController.java
+++ SERVLET-CRM/src/com/java/controller/HomeController.java
@@ -1,6 +1,8 @@
 package com.java.controller;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,11 +10,23 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.java.connection.JDBCConnection;
+
 @WebServlet("/home")
 public class HomeController extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try (Connection connection = JDBCConnection.getConnection()){
+			if (connection != null) {
+				System.out.println("Connect thanh cong");
+			} else {
+				System.out.println("Connect that bai");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		req.getRequestDispatcher("views/home/index.jsp").forward(req, resp);
 	}
 }
